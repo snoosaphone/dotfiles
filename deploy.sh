55765e8cf60d3422233e2768b6ba8558421c11d7
Joshua Finch <jfinch@faultybranches.net>
2024-03-04 14:08:47 -0600

    Moving all the nvim files to a subdirectory and updated the deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 # Neovim
-ln -fs $PWD/init.lua $HOME/.config/nvim/init.lua
-ln -fs $PWD/lua $HOME/.config/nvim/lua
+ln -fs $PWD/nvim/init.lua $HOME/.config/nvim/init.lua
+ln -fs $PWD/nvim/lua $HOME/.config/nvim/lua
 
 # Tmux
 ln -fs $PWD/tmux.conf $HOME/.tmux.conf
