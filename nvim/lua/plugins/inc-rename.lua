95a58ffb1ffe3d1199e0008a99ae9050f1c354b0
Joshua Finch <jfinch@faultybranches.net>
2024-03-06 12:32:45 -0600

    Adding the incremental rename plugin, though its workspace-wide rename needs to be figured out.

---
+++ nvim/lua/plugins/inc-rename.lua
@@ -0,0 +1,14 @@
+return {
+    'smjonas/inc-rename.nvim',
+    config = function()
+        require('inc_rename').setup()
+    end,
+    keys = {
+        {
+            '<leader>rn',
+            function()
+                return ':IncRename ' .. vim.fn.expand('<cword>')
+            end,
+        },
+    },
+}
