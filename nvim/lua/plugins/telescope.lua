68d00cde999c8ba501483b255dd70ee397f4b087
Joshua Finch <jfinch@faultybranches.net>
2024-04-02 09:42:44 -0500

    Reverting the change as it affects Telekasten's search functions by preventing the floating window from opening. Further testing needed.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,30 +3,30 @@ return {
     config = function(_, opts)
         local telescope = require('telescope')
         local actions = require('telescope.actions')
-        local action_state = require('telescope.actions.state')
+        -- local action_state = require('telescope.actions.state')
         local fb_actions = telescope.extensions.file_browser.actions
 
-        local custom_actions = {}
+        -- local custom_actions = {}
 
-        function custom_actions.grep_multi_select(prompt_bufnr)
-            local function get_table_size(t)
-                local count = 0
-                for _ in pairs(t) do
-                    count = count + 1
-                end
-                return count
-            end
+        -- function custom_actions.grep_multi_select(prompt_bufnr)
+            -- local function get_table_size(t)
+                -- local count = 0
+                -- for _ in pairs(t) do
+                    -- count = count + 1
+                -- end
+                -- return count
+            -- end
 
-            local picker = action_state.get_current_picker(prompt_bufnr)
-            local num_selections = get_table_size(picker:get_multi_selection())
+            -- local picker = action_state.get_current_picker(prompt_bufnr)
+            -- local num_selections = get_table_size(picker:get_multi_selection())
 
-            if num_selections > 1 then
-                actions.send_selected_to_qflist(prompt_bufnr)
-                actions.open_qflist()
-            else
-                actions.file_edit(prompt_bufnr)
-            end
-        end
+            -- if num_selections > 1 then
+                -- actions.send_selected_to_qflist(prompt_bufnr)
+                -- actions.open_qflist()
+            -- else
+                -- actions.file_edit(prompt_bufnr)
+            -- end
+        -- end
 
         opts.defaults = {
             file_ignore_patterns = {
@@ -43,11 +43,11 @@ return {
             layout_strategy = 'horizontal',
             mappings = {
                 n = {
-                    ['<cr>'] = custom_actions.grep_multi_select,
+                    -- ['<cr>'] = custom_actions.grep_multi_select,
                 },
                 i = {
                     ['<ESC>'] = actions.close,
-                    ['<cr>'] = custom_actions.grep_multi_select,
+                    -- ['<cr>'] = custom_actions.grep_multi_select,
                 },
             },
             prompt_prefix = '   ',
