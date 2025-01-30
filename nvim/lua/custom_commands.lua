87978e824b3642d5e371dcc0b5a59f378b36a10a
Joshua Finch <jfinch@faultybranches.net>
2025-01-29 20:03:41 -0600

    fix: Adding the global flag to the FindandReplace custom function.

--- nvim/lua/custom_commands.lua
+++ nvim/lua/custom_commands.lua
@@ -1,7 +1,7 @@
 vim.api.nvim_create_user_command('FindAndReplace', function(opts)
     -- Update each quickfix location using substitute, update the files to save changes, close the opened buffers and close the quickfix window
     -- TODO: Does not close the buffers opened through changes
-    vim.api.nvim_command(string.format('cdo s/%s/%s/', opts.fargs[1], opts.fargs[2]) .. '| update | cclose')
+    vim.api.nvim_command(string.format('cdo s/%s/%s/g', opts.fargs[1], opts.fargs[2]) .. '| update | cclose')
 end, { nargs = '*' })
 
 vim.api.nvim_set_keymap(
