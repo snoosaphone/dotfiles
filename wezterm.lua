454a32e8de0dc53e617dccff4023e40a8e35947f
Joshua Finch <jfinch@faultybranches.net>
2025-02-19 09:55:52 -0600

    Adding GTK xcursor settings to try and fix the cursor issues.

--- wezterm.lua
+++ wezterm.lua
@@ -1,12 +1,35 @@
 local wezterm = require 'wezterm'
 
+local xcursor_size = nil
+local xcursor_theme = nil
+
+local success, stdout, _ = wezterm.run_child_process({
+    "gsettings",
+    "get",
+    "org.gnome.desktop.interface",
+    "cursor-theme"
+})
+if success then
+    xcursor_theme = stdout:gsub("'(.+)'\n", "%1")
+end
+
+local success, _, _ = wezterm.run_child_process({
+    "gsettings",
+    "get",
+    "org.gnome.desktop.interface",
+    "cursor-size"
+})
+if success then
+    xcursor_size = tonumber(stdout)
+end
+
 local config = wezterm.config_builder()
 
-config.color_scheme = 'darkmatrix'
-config.color_scheme = 'Gruvbox dark, hard (base16)'
--- config.color_scheme = 's3r0 modified (terminal.sexy)'
-
-config.color_scheme = 'Mashup Colors (terminal.sexy)'
+-- config.color_scheme = 'darkmatrix'
+config.color_scheme = 's3r0 modified (terminal.sexy)'
+-- config.color_scheme = 'Mashup Colors (terminal.sexy)'
+-- config.color_scheme = 'Gruvbox dark, pale (base16)'
+config.color_scheme = 'Gruvbox Material (Gogh)'
 
 config.font = wezterm.font 'Inconsolata Nerd Font Mono'
 config.font_size = 12.5
@@ -20,5 +43,7 @@ config.window_padding = {
 }
 
 config.window_background_opacity = 0.97
+config.xcursor_theme = xcursor_theme
+config.xcursor_size = xcursor_size
 
 return config
